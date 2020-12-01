25df1e0782081f2255b14410e3b622629e1234f4
tarushjreddy <tarushjreddy63@gamil.com>
2020-12-01 13:56:50 +0530

    version one

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
+import { Todo } from './todo.js';
+import { NgModule } from '@angular/core';
 
+import { trigger } from '@angular/animations';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // animations: [
+  //   trigger('fade', [
+  //     transition('void => *', [
+  //       style({
+  //         backgroundColor: 'red',
+  //         opacity: 0,
+  //       }),
+  //       animate(2000),
+  //     ]),
+  //   ]),
+  // ],
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'Sunnone';
+  date = new Date();
+  name = 'cdcds';
+  todoValue: String;
+  list: Todo[];
+  ngOnInit() {
+    this.list = [];
+    this.todoValue = '';
+  }
+  addItem() {
+    if (this.todoValue != '') {
+      const newItem: Todo = {
+        id: Date.now(),
+
+        value: this.todoValue,
+        isDone: false,
+      };
+      this.list.push(newItem);
+    }
+    this.todoValue = '';
+  }
+  deleteItem(id: number) {
+    this.list = this.list.filter((item) => item.id != item.id);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
